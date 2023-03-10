7865d47ec1c3c9721fbd46f2bb58403ee6c05beb
Adriano Costa <adrianocostajhp@gmail.com>
2023-03-10 11:30:32 -0300

    ✨ Implement user and category module routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from "fastify";
+import categoryController from "../controllers/categoryController";
+import userController from "../controllers/userController";
 
-export const appRoutes = (app: FastifyInstance) => {
-  app.get("/", (req, res) => {
-    return res.send({ Hello: "word" });
-  });
+export const appRoutes = async (app: FastifyInstance) => {
 
-  app.get("/users", (req, res) => {
-    res.send({ Message: "Alou rota de users" });
-  });
+  app.get("/", (_, res) => res.send({ Hello: "word" }));
+
+  app.get("/users", userController.getUsers);
+
+  app.post("/users", userController.insertUser);
+
+  app.delete("/users/:id", userController.deleteUser);
+
+  app.patch("/users/:id", userController.updateUser);
+
+  app.get("/category", categoryController.getCategories);
+
+  app.post("/category", categoryController.insertCategory);
+
+  app.delete("/category/:id", categoryController.deleteCategory);
+
+  app.patch("/category/:id", categoryController.updateCategory);
+
   return app;
 };
